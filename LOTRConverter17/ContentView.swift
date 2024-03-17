8fd079d221b188ca7ffe1f87108ead62f3e8bcbd
Ruben Movsisyan <rubenmovsisyan@Rubens-MacBook-Pro-2.local>
2024-03-16 23:49:42 -0700

    first commit with some base code added to the project

---
+++ LOTRConverter17/ContentView.swift
@@ -0,0 +1,98 @@
+//
+//  ContentView.swift
+//  LOTRConverter17
+//
+//  Created by Ruben Movsisyan on 3/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var showExchangeInfo = false
+    @State var leftAmount = ""
+    @State var rightAmount = ""
+    
+    var body: some View {
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                
+                HStack {
+                    VStack {
+                        HStack {
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 33)
+                            
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        .padding(.bottom, -5)
+                        
+                        TextField( "Amount", text: $leftAmount)
+                            .textFieldStyle(.roundedBorder)
+                     }
+                    
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+                    VStack {
+                        HStack {
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 33)
+                        }
+                        .padding(.bottom,  -5)
+                        
+                        TextField( "Amount", text: $rightAmount)
+                            .textFieldStyle(.roundedBorder)
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
+                .padding()
+                .background(.black.opacity(0.5))
+                .clipShape(.capsule)
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        showExchangeInfo.toggle()
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.trailing)
+                }
+            }
+//            .border(.blue)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
